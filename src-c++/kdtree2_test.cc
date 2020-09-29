ef2e040e1c560d4505357214d8a196ecc67d5d1d
ming <ming.ustb@outlook.com>
2020-09-29 23:59:44 +0800

    - add bench mark result

--- src-c++/kdtree2_test.cc
+++ src-c++/kdtree2_test.cc
@@ -73,6 +73,22 @@ void time_random_searches(kdtree2::KDTree *tree, int nn)
   }
 }
 
+// Give me N, and dim (e.g. '1000 3').  No commas!1000 2
+// I found N=1000,dim=2
+//  rearranging
+// Tree created, now testing against brute force...
+// Testing complete.  Now testing timing...
+// C++ impl, for nn=1 searches/sec = 415082
+//  2.4092 us
+// C++ impl, for nn=5 searches/sec = 161667
+//  6.1855 us
+// C++ impl, for nn=10 searches/sec = 102175
+//  9.7871 us
+// C++ impl, for nn=25 searches/sec = 44185.9
+//  22.6316 us
+// C++ impl, for nn=500 searches/sec = 3195.09
+
+
 int main()
 {
   array2dfloat data(boost::extents[10][3]); // declare a 10000 x 3 array.
